020b77c2f0ea9e87f34d4c5d16d45c88e715e672
Manuel Muzembela <manuelmuzembela08@gmail.com>
2026-09-02 09:11:05 +0100

    remove from array done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr, el)
+{
+  const deleted = arr.filter(num => num !== el)
+  console.log(deleted)
+}
+const arr= [1,2,3,4]
+removeFromArray(arr, 3)
 // Do not edit below this line
 module.exports = removeFromArray;
