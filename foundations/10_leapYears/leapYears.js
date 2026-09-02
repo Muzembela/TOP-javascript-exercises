924f29db4c971eb838fb6a3e5414a410cf65dfcf
Manuel Muzembela <manuelmuzembela08@gmail.com>
2026-09-02 09:42:54 +0100

    leap years done

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if ((num % 4 || num % 400) === 0) 
+      return true
+    else
+      return false
 };
+
+leapYears(1985)
 
 // Do not edit below this line
 module.exports = leapYears;
