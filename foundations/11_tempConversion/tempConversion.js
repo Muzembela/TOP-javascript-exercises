10463a88320439f60c9313afa005666f2fd520c1
Manuel Muzembela <manuelmuzembela08@gmail.com>
2026-09-02 09:49:41 +0100

    done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  
+  return celsius = (f - 32) / 1.8
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return f = (c * 1.8) + 32
 };
 
+convertToCelsius(32)
+convertToFahrenheit(0) 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
