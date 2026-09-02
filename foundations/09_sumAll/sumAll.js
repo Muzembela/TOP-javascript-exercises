47bc80aa254fecfe300c4ef1819ceae5aff06840
Manuel Muzembela <manuelmuzembela08@gmail.com>
2026-09-02 09:38:06 +0100

    sum all done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let s = 0
+    while(a<=b)
+    {
+        s += a
+        a++
+    }
+  return s
 };
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
