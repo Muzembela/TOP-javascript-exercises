212e12e7667338efbe14f4282372c301b90aaa51
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-09-02 08:27:06 +0100

    repeat string done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let i = 0
+    let res = ""
+    while(i < num)
+    {
+        res += str
+        i++
+    }
+    console.log(res)
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+repeatString("hey", 2)
